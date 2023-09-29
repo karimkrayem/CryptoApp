e76ab6907778fae2eb0f72f3edcd17710be702d4
Karim <Krayem.karim@gmail.com>
2023-09-29 18:03:45 +0200

    Added header and Model

---
+++ CryptoApp/Model/CoinModel.swift
@@ -0,0 +1,54 @@
+//
+//  CoinModel.swift
+//  CryptoApp
+//
+//  Created by Karim on 29/09/2023.
+//
+
+import Foundation
+
+/*
+ 
+ CoinGuecko ApiInfo :
+ 
+ URL : https://api.coingecko.com/api/v3/coins/markets?vs_currency=usd&order=market_cap_desc&per_page=250&page=1&sparkline=true&price_change_percentage=24h
+ 
+ Api Response :
+ 
+ {
+     "id": "bitcoin",
+     "symbol": "btc",
+     "name": "Bitcoin",
+     "image": "https://assets.coingecko.com/coins/images/1/large/bitcoin.png?1547033579",
+     "current_price": 26872,
+     "market_cap": 524111706712,
+     "market_cap_rank": 1,
+     "fully_diluted_valuation": 564447906724,
+     "total_volume": 15381589003,
+     "high_24h": 27225,
+     "low_24h": 26744,
+     "price_change_24h": -75.536454659883,
+     "price_change_percentage_24h": -0.28031,
+     "market_cap_change_24h": -120657665.382568,
+     "market_cap_change_percentage_24h": -0.02302,
+     "circulating_supply": 19499312,
+     "total_supply": 21000000,
+     "max_supply": 21000000,
+     "ath": 69045,
+     "ath_change_percentage": -61.14828,
+     "ath_date": "2021-11-10T14:24:11.849Z",
+     "atl": 67.81,
+     "atl_change_percentage": 39459.76695,
+     "atl_date": "2013-07-06T00:00:00.000Z",
+     "roi": null,
+     "last_updated": "2023-09-29T15:53:42.327Z",
+     "sparkline_in_7d": {
+       "price": [26642.3499404449, 27049.5925501818]
+     },
+     "price_change_percentage_24h_in_currency": -0.280306149445451
+   }
+ */
+
+class CoinModel {
+    
+}
